440c293a9379ac81921a7bd9c94cedb3b53cbd2a
lora-yiting <55814203+lora-yiting@users.noreply.github.com>
2019-09-26 01:55:06 -0400

    Add files via upload

---
+++ Week2 story.py
@@ -0,0 +1,12 @@
+
+name = raw_input('Name of the player?: ')
+print(name + "Let us plan for your travel during this vocatino!") 
+
+cloth= raw_input("You are going to have vocatino. You may want to dress shirt or dress?: ")
+location= raw_input("You would be like to go to China or Australia?: ")
+pet= raw_input("You would be like to bring your pet with you,a cat or a dog?: ")
+color= raw_input("What is your favourite color?:")
+player= raw_input("Type any one you want to take with or meet on vocation?")
+place= raw_input("You preper to go shopping or go eating?:")
+
+print(name + "! Here is your travel plan! You are going to "+location+" for this vocation. Because you want to bring a "+pet+" with you, you may find it is hard to order a suitable room. However,"+color+" is really your lucky color!You will not only be paid for the trip by "+player+" but also find a really good place to stay during vocation.After you "+place+", you would find it can be the greatest holiday for you!")
