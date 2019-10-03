660bc0048e95f6bfa8f1a4ac2ba647d4b920edce
lora-yiting <55814203+lora-yiting@users.noreply.github.com>
2019-10-03 13:19:20 -0400

    Add files via upload

---
+++ choose nuber to change color and shape.py
@@ -0,0 +1,53 @@
+from turtle import *
+speed(1000)
+colors = ['red', 'purple', 'blue', 'green', 'yellow', 'orange',]
+number = raw_input('choose a number between 1 and 10: ')
+if number == "1":
+  # for loop for drawing left(150)
+  for x in range(360):
+    pencolor(colors[x % 6])
+    width(x / 100 + 1)
+    forward(x)
+    left(150)
+elif number == "2":
+  # for loop for drawing left(300)
+  for x in range(360):
+    pencolor(colors[x % 6])
+    width(x / 100 + 1)
+    forward(x)
+    left(175)
+elif number == "3":
+  # for loop for drawing left(300)
+  for x in range(360):
+    pencolor(colors[x % 6])
+    width(x / 100 + 1)
+    forward(x)
+    left(300)
+elif number == "4":
+  # for loop for drawing left(300)
+  for x in range(360):
+    pencolor(colors[x % 6])
+    width(x / 100 + 1)
+    forward(x)
+    left(450)
+elif number == "5":
+  # for loop for drawing left(300)
+  for x in range(360):
+    pencolor(colors[x % 6])
+    width(x / 100 + 1)
+    forward(x)
+    left(500)
+elif number == "6":
+  # for loop for drawing left(300)
+  for x in range(360):
+    pencolor(colors[x % 6])
+    width(x / 100 + 1)
+    forward(x)
+    left(600)
+elif number == "7":
+  # for loop for drawing left(300)
+  for x in range(360):
+    pencolor(colors[x % 6])
+    width(x / 100 + 1)
+    forward(x)
+    left(750)
